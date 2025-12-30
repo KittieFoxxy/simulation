31eab662eee287b93d044b468b74d4da5b7c0c7b
cat <catlisichkina@gmail.com>
2025-12-30 16:28:15 +0300

    Project set up

---
+++ src/main/java/com/pot/Main.java
@@ -0,0 +1,7 @@
+package com.pot;
+
+public class Main {
+    public static void main(String[] args) {
+        new Simulation().start();
+    }
+}
---
+++ src/main/java/com/pot/Simulation.java
@@ -0,0 +1,8 @@
+package com.pot;
+
+public class Simulation {
+
+    public void start() {
+        System.out.println("Starting Simulation");
+    }
+}
